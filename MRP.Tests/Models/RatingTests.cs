662ce3124614bccdb5038be0bcc7aec691a73219
DESKTOP-8NQH97N\Melanie <mel.reithmaier@gmx.at>
2025-12-03 21:27:06 +0100

    Add tests: CreatingRating_CommentOptionalAndTrimmed

--- MRP.Tests/Models/RatingTests.cs
+++ MRP.Tests/Models/RatingTests.cs
@@ -18,5 +18,24 @@ namespace MRP.Tests
                 _ = new RatingEntry("Inception", "melanie", 6, null);
             });
         }
+
+        [TestMethod]
+        public void CreatingRating_ShouldSetCreatedAt()
+        {
+            var before = DateTime.UtcNow;
+            var rating = new RatingEntry("Inception", "melanie", 3, null);
+            var after = DateTime.UtcNow;
+            Assert.IsTrue(rating.CreatedAt >= before && rating.CreatedAt <= after);
+        }
+
+        [TestMethod]
+        public void CreatingRating_CommentOptionalAndTrimmed()
+        {
+            var ratingWithoutComment = new RatingEntry("Inception", "melanie", 3, null);
+            Assert.IsNull(ratingWithoutComment.Comment);
+
+            var ratingWithComment = new RatingEntry("Inception", "melanie", 3, "  nice movie  ");
+            Assert.AreEqual("nice movie", ratingWithComment.Comment);
+        }
     }
 }
